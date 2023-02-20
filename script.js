45735ff1beb43dfa92409c189647dfb89d4fc434
Sabir-itobuz <sabir@itobuz.com>
2023-02-20 17:48:07 +0530

    20  feb

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    grabCursor: true,
-    autoplay: {
-        delay:2500,
-        disableOnInteraction:false,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centeredSlides: true,
+  freeMode: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
 
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    
-      clickable: true,
-    },
-  });
+
+let price = document.querySelectorAll(".plan")
+
+
+
+price.forEach((card) => {
+  card.addEventListener("mouseover", () => {
+    console.log(card.firstElementChild);
+    card.firstElementChild.style.color = "white"
+  })
+})
+price.forEach((card) => {
+  card.addEventListener("mouseout", () => {
+    card.firstElementChild.style.color = "#6f77f1"
+  })
+})
+
+price.forEach((button) => {
+  button.addEventListener("mouseover", () => {
+    button.lastElementChild.style.backgroundColor = "#FAD725"
+  })
+})
+price.forEach((button) => {
+  button.addEventListener("mouseout", () => {
+    button.lastElementChild.style.backgroundColor = "white"
+  })
+})
