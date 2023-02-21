2abce89b742793ed87cbedeb274fad2c701d957b
Sabir-itobuz <sabir@itobuz.com>
2023-02-21 12:51:17 +0530

    21  feb

--- script.js
+++ script.js
@@ -20,29 +20,3 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
-let price = document.querySelectorAll(".plan")
-
-
-
-price.forEach((card) => {
-  card.addEventListener("mouseover", () => {
-    console.log(card.firstElementChild);
-    card.firstElementChild.style.color = "white"
-  })
-})
-price.forEach((card) => {
-  card.addEventListener("mouseout", () => {
-    card.firstElementChild.style.color = "#6f77f1"
-  })
-})
-
-price.forEach((button) => {
-  button.addEventListener("mouseover", () => {
-    button.lastElementChild.style.backgroundColor = "#FAD725"
-  })
-})
-price.forEach((button) => {
-  button.addEventListener("mouseout", () => {
-    button.lastElementChild.style.backgroundColor = "white"
-  })
-})
